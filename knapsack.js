82c95e7dfe2b834649bdba8c3677a2aa6aedb1d8
stanri <sarivera@mit.edu>
2014-06-17 14:12:20 -0400

    fully functional, with a few bugs here and there (specifically, draw has not been tested)

--- knapsack.js
+++ knapsack.js
@@ -1,50 +1,66 @@
-function updateKnapsack() {
-
-}
-
 $(function() {
     var items = $('.item');
     var weight = 0;
     var value = 0;
     var maxWeight = parseInt($(".mainContainer").attr('data-maxweight'));
-    console.log(maxWeight);
     for (i = 0; i < items.length; i++) {
-        console.log(items[i]);
-        items[i].data('location', 'house');
+        $(items[i]).data('location', 'house'); //never forget, you need the selector!
     }
 
     function exceededCapacity() {
-        $('.alert').fadeIn(500);
-        $('.alert').fadeOut(500);
+        console.log('exceeded capacity');
+        $('.alert').fadeIn(2000);
+        $('.alert').fadeOut(2000);
+    }
+
+    function updateKnapsack() {
+        $("#knapsack .header .info").text("($" + value + ", " + weight + "kg)");
+    }
+
+    function draw() {
+        $(".items").empty;
+        for (i = 0; i < items.length; i++) {
+            if ($(items[i]).data('location') == 'house') {
+                $("#house .items").append($(items[i]))
+            }
+            else {
+                $("#knapsack .items").append($(items[i]))
+            }
+        }
     }
 
     function steal(stealMe) {
-        var newWeight = weight + parseInt($(stealMe).attr('data-value'));
+        var newWeight = weight + parseInt($('img', stealMe).attr('data-weight'));
         if (newWeight > maxWeight) {exceededCapacity();}
         else {
             //move item to the knapsack
-            value += parseInt($(stealMe).attr('data-value'));
+            value += parseInt($('img', stealMe).attr('data-value'));
             weight = newWeight;
-            stealMe.remove();
-            $("#knapsack").append(stealMe);
+            //stealMe.detach();
+            //$("#knapsack .items").append(stealMe);
             $(stealMe).data('location', 'knapsack');
+            updateKnapsack();
+            draw();
         }
     }
 
     function unsteal(replaceMe) {
-        weight -= parseInt($(replaceMe).attr('data-value'));
+        weight -= parseInt($('img', replaceMe).attr('data-weight'));
         //move item to the house
-        value -= parseInt($(replaceMe).attr('data-value'));
-        replaceMe.remove();
-        $("#house").append(replaceMe);
+        value -= parseInt($('img', replaceMe).attr('data-value'));
+        //replaceMe.detach();
+        //$("#house .items").append(replaceMe);
         $(replaceMe).data('location', 'house');
+        updateKnapsack();
+        draw();
     }
 
     items.click(function(event) {
-        if ($(this).data('location') === 'house') {
+        console.log("clicked item is in: " + $(this).data('location'));
+        if ($(this).data('location') == 'house') {
             steal($(this));
         }
-        else if ($(this).data('location') === 'knapsack') {
+        else if ($(this).data('location') == 'knapsack') {
             unsteal($(this));
         }
         $("#knapsack.header").text("($" + value + ", " + weight + "kg)");
